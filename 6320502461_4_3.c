6db8fa0f2edc83525a61b43fa01f2c490ccfcde5
MamoruTFN <paweenwich.t@ku.th>
2021-03-04 16:32:44 +0700

    dirtykeyboard

--- 6320502461_4_3.c
+++ 6320502461_4_3.c
@@ -2,6 +2,16 @@
 #include<string.h>
 int main()
 {
-
+    int i;
+    char c[200];
+    scanf("%s",c);
+    for(i=0;i<strlen(c)-1;i++)
+    {
+        if(c[i]!=c[i+1])
+        {
+            printf("%c",c[i]);
+        }
+    }
+    printf("%c",c[strlen(c)-1]);
     return 0;
 }
